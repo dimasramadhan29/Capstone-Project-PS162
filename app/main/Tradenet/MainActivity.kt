93b7f2af464da20723d048d77e0690954ec16828
Rahmat_Ferdy <79414122+ansilvirissa29@users.noreply.github.com>
2023-06-16 16:11:36 +0700

    Add files via upload

--- app/main/Tradenet/MainActivity.kt
+++ app/main/Tradenet/MainActivity.kt
@@ -1,1 +1,24 @@
+package com.c23_ps162.trade_net
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c23_ps162.trade_net.util.pattern.Bootstrap
+
+class MainActivity : AppCompatActivity(), Bootstrap {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initState()
+    }
+
+    override fun initState() {
+        initNavigation()
+    }
+
+    override fun initNavigation() {
+
+    }
+
+}
