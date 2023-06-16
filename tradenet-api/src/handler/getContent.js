b051775e061a58bed7ff5835251faf05bd2098f4
rizki0987 <113367468+rizki0987@users.noreply.github.com>
2023-06-16 19:05:56 +0700

    api

---
+++ tradenet-api/src/handler/getContent.js
@@ -0,0 +1,65 @@
+const db = require('../config/db');
+
+const getContent = (request, h) => {
+  const { q } = request.query;
+
+  // Mengatur nilai default untuk q jika tidak ada input dari pengguna
+  const qValue = q || '';
+
+  // Query untuk mencari postingan berdasarkan caption dari tabel content
+  const query = `
+    SELECT
+      content.userId,
+      users.userProfilePicture AS userProfilePictureSmallURL,
+      content.contentId,
+      content.imageURL,
+      content.title,
+      content.caption,
+      content.likeCount,
+      content.liked,
+      content.priceItem,
+      CONCAT('Rp.', content.priceItem) AS priceItemPrintAble
+    FROM
+      content
+    INNER JOIN
+      users ON content.userId = users.id
+    WHERE
+      content.caption LIKE '%${qValue}%';
+  `;
+
+  return new Promise((resolve, reject) => {
+    db.query(query, (error, results, fields) => {
+      if (error) {
+        console.error('Gagal mengambil postingan: ', error);
+        reject({
+          success: false,
+          error: 'Terjadi kesalahan saat mengambil postingan',
+        });
+      } else {
+        resolve({
+          success: true,
+          data: {
+            list: results.map((result) => ({
+              creatorProfile: {
+                userId: result.userId,
+                userProfilePictureSmallURL: result.userProfilePictureSmallURL,
+              },
+              content: {
+                contentId: result.contentId,
+                imageURL: result.imageURL,
+                title: result.title,
+                caption: result.caption,
+                likeCount: result.likeCount,
+                liked: result.liked,
+                priceItem: result.priceItem,
+                priceItemPrintAble: result.priceItemPrintAble,
+              },
+            })),
+          },
+        });
+      }
+    });
+  });
+};
+
+module.exports = getContent;
